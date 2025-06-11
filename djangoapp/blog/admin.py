f8e35c68267f9799a08986ad1bd3c3ba363667c4
GregilGilmar <gil128mar@gmail.com>
2025-06-11 18:37:37 -0300

    Iniciando a parte interna do Post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,7 @@
-from blog.models import Category, Page, Post, Tag 
+from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Tag)
@@ -11,9 +13,9 @@ class TagAdmin(admin.ModelAdmin):
     ordering = '-id',
     prepopulated_fields = {
         "slug": ('name',),
-     }
- 
- 
+    }
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
@@ -23,42 +25,58 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = '-id',
     prepopulated_fields = {
         "slug": ('name',),
-     }
+    }
 
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'content',
     list_per_page = 50
     list_filter = 'is_published',
     list_editable = 'is_published',
-    odering = '-id',
+    ordering = '-id',
     prepopulated_fields = {
         "slug": ('title',),
     }
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'is_published', 'created_by',
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = 'id', 'title', 'is_published',  'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
     list_per_page = 50
     list_filter = 'category', 'is_published',
-    list_edtable = 'is_published',
+    list_editable = 'is_published',
     ordering = '-id',
-    reandonly_fields = 'created_at', 'update_at', 'created_by', 'update_by',
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by',
+        'link',
+    )
     prepopulated_fields = {
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
 
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+
+        url_do_post = obj.get_absolute_url()
+        safe_link = mark_safe(
+            f'<a target="_blank" href="{url_do_post}">Ver post</a>'
+        )
+
+        return safe_link
+
     def save_model(self, request, obj, form, change):
         if change:
-            obj.updated_by = request.user
+            obj.updated_by = request.user  # type: ignore
         else:
-            obj.created_by = request.user
-        
+            obj.created_by = request.user  # type: ignore
+
         obj.save()
